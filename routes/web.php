a7b1a62201c6d53313621cd4b349a543f8f18080
luiz-jolo <luizfelipe.jolo@gmail.com>
2020-04-22 10:32:04 -0400

    Adicao do Sweet Alert

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use RealRashid\SweetAlert\Facades\Alert;
 
 Route::get('/', function () {
+    Alert::success('Success title', 'Success Message');
     return view('welcome');
 });
 
